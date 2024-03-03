1301616a6331304797320c4eacaee14cbe66e791
Diego Garcia <diegogarcia@netrixllc.com>
2024-03-02 21:12:55 -0300

    adding local storage

--- src/server.py
+++ src/server.py
@@ -6,11 +6,20 @@ import sales_pb2_grpc
 import logging
 from concurrent import futures
 
+sales_data = []
+
 class SalesService(sales_pb2_grpc.SalesServiceServicer):
     def ProcessSale(self, request, context):
+        global sales_data
+        sales_data.append({
+            "item": request.item,
+            "quantity": request.quantity,
+            "price": request.price,
+            "date": request.date
+        })
         print(f"Sale: item {request.item}, quantity {request.quantity}, price {request.price}, date {request.date}")
         return sales_pb2.ConfirmationReply(
-            message=f"Sale: item {request.item}, quantity {request.quantity}, price {request.price}, date {request.date}"
+            message=f"Sale: item {request.item}, quantity {request.quantity}, price {request.price}, date {request.date}. Sales data: {sales_data}"
         )
 
 def serve():
